a1ee19846db28c41b3b137494d9059142cf0558c
Norton Oliveira <norton_rayan@hotmail.com>
2024-11-20 11:39:44 -0300

    feat: projeto rodando -zerado

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { View } from 'react-native'
+import { Button, View } from 'react-native'
 
 export function App(){
 
 return (
   <View style={{flex:1,backgroundColor:"#222"}}>
-
+    <Button title='teste'/>
   </View>
   )
 }
