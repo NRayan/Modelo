6afdf35283d493aaa64705a74bea09895bb51c38
Norton Rayan <norton_rayan@hotmail.com>
2024-11-21 15:14:35 -0300

    feat: tensorflow funcional

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,37 +1,56 @@
-import { Button, View, Text, Image } from 'react-native'
+import '@tensorflow/tfjs-react-native';
+import { Button, View, Text, Image } from 'react-native';
 import {fetch, decodeJpeg} from '@tensorflow/tfjs-react-native';
 import * as tf from '@tensorflow/tfjs';
 import * as mobilenet from '@tensorflow-models/mobilenet';
-import { useRef, useState } from 'react';
+import { useRef, useState,useEffect } from 'react';
 
 export function Home(){
-  
+
   const [isTfReady, setIsTfReady] = useState(false);
+  const [classifying, setClassifying] = useState(false);
   const [result, setResult] = useState('');
   const image = useRef(null);
-  
+  const model = useRef<any>(null);
+
+  useEffect(()=>{
+    async function loadModel()
+    {
+      // Load mobilenet.
+      console.log('1');
+      await tf.ready();
+      console.log(fetch);
+
+      const t2 = performance.now();
+      const md = await mobilenet.load();
+      const t1 = performance.now();
+
+      console.log(`It took: ${((t2 - t1) / 1000).toFixed(4)}s`);
+      model.current = md;
+      setIsTfReady(true);
+    }
+    loadModel();
+  },[]);
+
   async function load() {
     try {
-      // Load mobilenet.
-      console.log("1")
-      await tf.ready();
-    //   console.log("2")
-    //   await tf.setBackend('cpu'); // Certifique-se de usar o backend correto
-      console.log("3")
-      const model = await mobilenet.load();
-      console.log("4")
-      setIsTfReady(true);
-
-      console.log("5")
+      setClassifying(true);
       // Start inference and show result.
-      const image = require('../../basketball.jpg');
-      const imageAssetPath = Image.resolveAssetSource(image);
+      const img = require('../../dog.jpg');
+      const imageAssetPath = Image.resolveAssetSource(img);
       console.log(`imageAssetPath: ${imageAssetPath}`);
+
       const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
       const imageDataArrayBuffer = await response.arrayBuffer();
       const imageData = new Uint8Array(imageDataArrayBuffer);
       const imageTensor = decodeJpeg(imageData);
-      const prediction = await model.classify(imageTensor);
+
+      console.log('6');
+
+      const t1 = performance.now();
+      const prediction = await model.current.classify(imageTensor);
+      const t2 = performance.now();
+      console.log(`It took: ${((t2 - t1) / 1000).toFixed(4)}s`);
 
       if (prediction && prediction.length > 0) {
         setResult(
@@ -40,8 +59,11 @@ export function Home(){
       }
     } catch (err) {
       console.log(err);
+    } finally
+    {
+      setClassifying(false);
     }
-  };
+  }
 
   return (
     <View
@@ -52,14 +74,14 @@ export function Home(){
         alignItems: 'center',
         justifyContent: 'center',
       }}>
-        <Button title='load' onPress={load}/>
+        <Button title="classify" onPress={load}/>
       <Image
         ref={image}
-        source={require('../../basketball.jpg')}
+        source={require('../../dog.jpg')}
         style={{width: 200, height: 200}}
       />
       {!isTfReady && <Text>Loading TFJS model...</Text>}
-      {isTfReady && result === '' && <Text>Classifying...</Text>}
+      {classifying && <Text>Classifying...</Text>}
       {result !== '' && <Text>{result}</Text>}
     </View>
   );
